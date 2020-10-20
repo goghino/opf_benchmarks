3939bd9a6a2887a940f9526aa7b60c1ec0367543
Juraj Kardoš <goghino@users.noreply.github.com>
2020-10-20 15:19:28 +0200

    Update adjust_ipopt_opt.m

--- toleranceBench/adjust_ipopt_opt.m
+++ toleranceBench/adjust_ipopt_opt.m
@@ -1,9 +1,11 @@
 function adjust_ipopt_opt(tol)
     A = regexp( fileread('beltistos.opt'), '\n', 'split');
-    A{17} = ['dual_inf_tol   '   num2str(tol)];
-    A{18} = ['compl_inf_tol    ' num2str(tol)];
-    A{19} = ['constr_viol_tol    ' num2str(tol)];
-    A{20} = ['tol    '        num2str(tol)];
+    A{12} = ['dual_inf_tol   '   num2str(tol)];
+    A{13} = ['compl_inf_tol    ' num2str(tol)];
+    A{14} = ['constr_viol_tol    ' num2str(tol)];
+    A{15} = ['tol    '        num2str(tol)];
+    A{33} = ['acceptable_constr_viol_tol   '   num2str(tol/5)];
+    A{34} = ['acceptable_tol   '   num2str(tol/5)];
     a = A(1:end-1);
 
     fid = fopen('beltistos.opt', 'w');
